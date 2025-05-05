274d5d6653cf39642eb1c835491856be8d97e47f
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2025-05-05 15:07:11 +0500

    update models

--- models/models.py
+++ models/models.py
@@ -5,13 +5,13 @@ class User(models.Model):
     Id = models.BigAutoField(primary_key=True)
 
     TgId = models.BigIntegerField(null=True, blank=True)
-    TgName = models.TextField(null=True, blank=True)
+    TgName = models.CharField(max_length=200, null=True, blank=True)
 
-    PhoneNumber = models.CharField(max_length=20, null=True, blank=True, unique=True)
-    Name = models.CharField(max_length=50, null=True, blank=True)
+    PhoneNumber = models.CharField(max_length=50, null=True, blank=True, unique=True)
+    Name = models.CharField(max_length=200, null=True, blank=True)
 
     TaskCount = models.IntegerField(default=0, null=True, blank=True)
-    Role = models.CharField(max_length=20, null=True, blank=True)
+    Role = models.CharField(max_length=50, null=True, blank=True)
 
     CreatedAt = models.DateTimeField(auto_now_add=True)
     UpdatedAt = models.DateTimeField(auto_now=True)
@@ -19,7 +19,7 @@ class User(models.Model):
     class Meta:
         db_table = "Users"
 
-    def __str__(self):
+    def __repr__(self):
         return f"User(Id={self.Id}, TgName='{self.TgName}')"
 
 
@@ -27,18 +27,18 @@ class Task(models.Model):
     Id = models.BigAutoField(primary_key=True)
 
     RequestDate = models.DateTimeField(null=True, blank=True)
-    Company = models.TextField(null=True, blank=True)
+    Company = models.CharField(max_length=200, null=True, blank=True)
 
-    Author = models.TextField(null=True, blank=True)
+    Author = models.CharField(max_length=200, null=True, blank=True)
     Message = models.TextField(null=True, blank=True)
 
-    Worker = models.TextField(null=True, blank=True)
+    Worker = models.CharField(max_length=200, null=True, blank=True)
 
     StartDate = models.DateTimeField(null=True, blank=True)
     EndDate = models.DateTimeField(null=True, blank=True)
 
-    IsReassigned = models.CharField(max_length=5, null=True, blank=True)
-    Confirmation = models.CharField(max_length=5, null=True, blank=True)
+    IsReassigned = models.CharField(max_length=10, null=True, blank=True)
+    Confirmation = models.CharField(max_length=10, null=True, blank=True)
 
     GroupId = models.BigIntegerField(null=True, blank=True)
     MessageId = models.BigIntegerField(null=True, blank=True)
@@ -52,7 +52,7 @@ class Task(models.Model):
     class Meta:
         db_table = "Tasks"
 
-    def __str__(self):
+    def __repr__(self):
         return f"Task(Id={self.Id}, Company='{self.Company}')"
 
 
@@ -60,15 +60,15 @@ class TaskReassign(models.Model):
     Id = models.BigAutoField(primary_key=True)
     TaskId = models.BigIntegerField(null=True, blank=True)
 
-    Appointer = models.TextField(null=True, blank=True)
-    Executor = models.TextField(null=True, blank=True)
+    Appointer = models.CharField(max_length=200, null=True, blank=True)
+    Executor = models.CharField(max_length=200, null=True, blank=True)
 
     Hours = models.FloatField(null=True, blank=True)
 
     StartDate = models.DateTimeField(null=True, blank=True)
     EndDate = models.DateTimeField(null=True, blank=True)
 
-    IsDone = models.CharField(max_length=5, null=True, blank=True)
+    IsDone = models.CharField(max_length=10, null=True, blank=True)
 
     AppointerId = models.BigIntegerField(null=True, blank=True)
     ExecutorId = models.BigIntegerField(null=True, blank=True)
@@ -79,5 +79,5 @@ class TaskReassign(models.Model):
     class Meta:
         db_table = "TaskReassigns"
 
-    def __str__(self):
+    def __repr__(self):
         return f"TaskReassign(Id={self.Id}, TaskId={self.TaskId})"
